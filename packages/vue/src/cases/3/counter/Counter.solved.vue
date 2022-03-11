f49d6629f71fa088689986920d7861937aec2785
Jakob Kruse <contact@jakob-kruse.de>
2022-03-11 18:47:32 +0100

    Add Solutions for Counter and Model

---
+++ packages/vue/src/cases/3/counter/Counter.solved.vue
@@ -0,0 +1,26 @@
+<!-- 
+    Write the script for a simple counter component.
+
+    The component should have a button that increments the counter
+
+    It should start at 0
+-->
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0)
+
+function increaseCounter() {
+    counter.value++
+}
+
+function decreaseCounter() {
+    counter.value--
+}
+</script>
+
+<template>
+    <p id="counter">Counter: {{ counter }}</p>
+    <button id="increase" @click="increaseCounter">Increase Counter</button>
+    <button id="decrease" @click="decreaseCounter">Decrease Counter</button>
+</template>
---
+++ packages/vue/src/cases/3/model/Model.solved.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref('')
+</script>
+
+<template>
+    <label for="username">Username</label>
+    <input id="input" type="text" name="username" placeholder="Enter username" v-model="username" />
+
+    <p id="greeting">Greetings {{ username || 'Unknown' }}</p>
+</template>
